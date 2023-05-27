344d3a2c2d3454cd9b6bc4de45f68fd671a3c5ae
Gabriel Oliveira <gabrielbarbieri430@gmail.com>
2023-05-26 22:41:17 -0300

    memes generator using React and SASS

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Header.sass"
+
+
+export default function Nav() {
+    return (
+        <header>
+            <div>
+                <img src="https://freepngimg.com/download/meme/98271-meme-face-derp-png-file-hd.png" />
+                <h1>Gerador de Memes</h1>
+            </div>
+            <p>
+                Crie seu próprio meme e viralize!
+            </p>
+        </header>
+    )
+}
